270140eb3acb9c376d4f6daa5596fbf4e63ce889
Harishankar G <harishankargaddanakeri@gmail.com>
2023-09-11 19:19:29 +0530

    Rewrite and simplify

--- wordle.js
+++ wordle.js
@@ -1,179 +1,179 @@
-let word, splitWords;
-let tries = 0;
-let position = 0;
-const attempts = [[], [], [], [], [], []];
-let containers = document.querySelectorAll(".trial");
-let backspaced = false;
+const letters = document.querySelectorAll(".letter"); // Array of letters
+const loadingDiv = document.querySelector(".indicator");
+const loadingSymbols = document.querySelector(".dot");
+
+const MAX_LENGTH = 5;
+const MAX_ROUNDS = 5;
+
+let row = 0;
+let guess = "";
+let word;
+let wordArray;
+
+let done = false;
+let isLoading;
 
 init();
 
-function init() {
-    (async () => {
-        [word, splitWords] = await getWord();
-    })();
-    clearTrials();
-    document
-        .querySelector(".wordle-game")
-        .addEventListener("keyup", function (InputEvent) {
-            handleKeyStroke(InputEvent);
-        });
+async function init() {
+    isLoading = true;
+    setLoading(isLoading);
+
+    // Get today's word
+    word = await getWord();
+    wordArray = word.split("");
+    wordCount = getCount(word);
+
+    isLoading = false;
+    setLoading(isLoading);
+
+    document.addEventListener("keydown", function handleKeyPress(InputEvent) {
+        if (done || isLoading) {
+            return; // do nothing
+        }
+
+        const action = InputEvent.key;
+
+        if (action === "Enter") {
+            submit();
+        } else if (action === "Backspace") {
+            backspace();
+        } else if (isLetter(action)) {
+            inputLetter(action.toUpperCase());
+        } else {
+            // do nothing
+        }
+    });
 }
 
 async function getWord() {
-    const res = await fetch("https://words.dev-apis.com/word-of-the-day");
-    const processed = await res.json();
-    word = processed["word"].toUpperCase();
-    return [word, word.split("")];
+    const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const json = await promise.json();
+    return json.word.toUpperCase();
 }
 
-async function isAWord(array) {
-    const res = await fetch("https://words.dev-apis.com/validate-word", {
+function setLoading(isLoading) {
+    loadingDiv.classList.toggle("hidden", !isLoading);
+    loadingSymbols.classList.toggle("upndowndot", isLoading);
+}
+
+function isLetter(key) {
+    return /^[a-zA-Z]$/.test(key);
+}
+
+function inputLetter(letter) {
+    if (guess.length < MAX_LENGTH) {
+        guess += letter;
+        letters[5 * row + guess.length - 1].innerText = letter; // Emulate a 6x5 grid
+    } else {
+        // do nothing
+    }
+}
+
+async function submit() {
+    if (guess.length != MAX_LENGTH) {
+        // do nothing
+        return;
+    } else {
+        const isValid = await validateWord(guess);
+        if (!isValid) {
+            markInvalidWord();
+            return;
+        }
+
+        const guessArray = guess.split("");
+        const wordCount = getCount(word);
+
+        // Set any correct letters to correct
+        for (let i = 0; i < MAX_LENGTH; i++) {
+            if (guessArray[i] === wordArray[i]) {
+                letters[row * MAX_LENGTH + i].classList.add("correct");
+                wordCount[guessArray[i]]--; // Reduce the count of letter
+            }
+        }
+
+        // Set any close or wrong letters to close or wrong
+        for (let i = 0; i < MAX_LENGTH; i++) {
+            if (guessArray[i] === wordArray[i]) {
+                // do nothing
+            } else if (wordCount[guessArray[i]] > 0) {
+                letters[row * MAX_LENGTH + i].classList.add("close");
+                wordCount[guessArray[i]]--;
+            } else {
+                letters[row * MAX_LENGTH + i].classList.add("wrong");
+            }
+        }
+
+        // Win
+        if (guess === word) {
+            isLoading = true;
+            loadingDiv.innerHTML = "You Win!";
+            loadingDiv.style = "font-family: Inter; font-size: 48px;";
+            setLoading(isLoading);
+
+            done = true;
+            return;
+        }
+
+        // Lose
+        if (row >= MAX_ROUNDS) {
+            isLoading = true;
+            loadingDiv.innerHTML = `You Lose! The word is ${word}`;
+            loadingDiv.style = "font-family: Inter; font-size: 48px;";
+            setLoading(isLoading);
+            done = true;
+            return;
+        }
+
+        row++; // Move to next row
+        guess = ""; // New attempt
+    }
+}
+
+async function validateWord(guess) {
+    isLoading = true;
+    setLoading(isLoading);
+
+    const promise = await fetch("https://words.dev-apis.com/validate-word", {
         method: "POST",
-        body: JSON.stringify({ word: array.join("") }),
+        body: JSON.stringify({ word: guess }),
     });
-    const processed = await res.json();
-    const boolean = processed["validWord"];
-    return boolean;
+
+    const json = await promise.json();
+
+    isLoading = false;
+    setLoading(isLoading);
+
+    return json.validWord;
 }
 
-function clearTrials() {
-    let trials = document.querySelectorAll(".letter");
-    for (let i = 0; i < trials.length; i++) {
-        trials[i].value = "";
+function markInvalidWord() {
+    for (let i = 0; i < MAX_LENGTH; i++) {
+        letters[row * MAX_LENGTH + i].classList.add("invalid");
+        setTimeout(function () {
+            letters[row * MAX_LENGTH + i].classList.remove("invalid");
+        }, 500);
     }
 }
 
-async function handleKeyStroke(input) {
-    if (isLetter(input.key) === true) {
-        handleLetter(input.key);
-    } else if (input.key === "Enter") {
-        await handleEnter();
-    } else if (input.key === "Backspace") {
-        handleBackspace();
+function backspace() {
+    if (guess.length > 0) {
+        letters[5 * row + guess.length - 1].innerText = "";
+        guess = guess.substring(0, guess.length - 1);
     } else {
-        input.preventDefault();
+        // do nothing
     }
 }
 
-function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
-}
-
-function handleLetter(letter) {
-    letter = letter.toUpperCase();
-    backspaced = false;
-
-    if (position === 5) {
-        return; // Validate word only on pressing enter key
-    } else {
-        attempts[tries][position] = letter;
-        if (position >= 0 && position < 4 && tries < 6) {
-            // Move to the next cell
-            containers[tries][position + 1].focus();
-            containers[tries][position + 1].value = "";
-            position++;
-        }
-    }
-}
-
-function makeReadOnly(tries) {
-    for (let i = 0; i < 5; i++) {
-        containers[tries][i].disabled = true;
-    }
-}
-
-async function handleEnter() {
-    let result, hints;
-
-    if (
-        attempts[tries].length === 5 &&
-        tries <= 5 &&
-        (await isAWord(attempts[tries]))
-    ) {
-        [result, hints] = validateWord(attempts[tries]);
-    } else {
-        alert("Enter a valid five letter word!");
-        return;
-    }
-
-    showHints(hints);
-    makeReadOnly(tries);
-    if (hints === "11111") {
-        alert("Congrats You Win!");
-        return;
-    }
-
-    // Move to the next row
-    tries++;
-    position = 0;
-
-    // Game over
-    if (tries > 5) {
-        alert(`Game Over! The word is ${word}`);
-        return;
-    }
-
-    containers[tries][position].focus();
-    containers[tries][position].value = "";
-}
-
-function validateWord(array) {
-    let hints = "";
-    let found = [];
-    for (let i = 0; i < 5; i++) {
-        if (array[i] === splitWords[i]) {
-            hints += "1";
-            found.push(array[i]);
-        } else if (
-            splitWords.includes(array[i]) &&
-            found.filter((x) => x === array[i]).length <
-                splitWords.filter((x) => x === array[i]).length
-        ) {
-            hints += "2";
-            found.push(array[i]);
+function getCount(array) {
+    const count = {};
+    for (let i = 0; i < array.length; i++) {
+        if (count[array[i]] > 0) {
+            count[array[i]] += 1;
         } else {
-            hints += "0";
+            count[array[i]] = 1;
         }
     }
-    return [true, hints];
-}
 
-function showHints(hints) {
-    let color = "";
-    for (let i = 0; i < 5; i++) {
-        if (hints[i] === "1") {
-            color = "green";
-        } else if (hints[i] === "2") {
-            color = "yellow";
-        } else {
-            color = "red";
-        }
-
-        containers[tries][i].style.backgroundColor = color;
-    }
-}
-
-function handleBackspace() {
-    if (position === 0) {
-        alert("No letters to clear!");
-    } else if (backspaced === true || isEmptyOrUndefined(tries, position)) {
-        position--;
-        containers[tries][position].focus();
-        containers[tries][position].value = "";
-    } else {
-        backspaced = true;
-        return;
-    }
-}
-
-function isEmptyOrUndefined(tries, position) {
-    // Handle wierd behaviour of attempts[tries][position] being sometimes "" or undefined or my stupidness
-    if (attempts[tries][position] === undefined) {
-        return true;
-    }
-    if (attempts[tries][position].value === "") {
-        return true;
-    }
-
-    return false;
+    return count;
 }
